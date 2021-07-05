2ef63b4ee762152ae70d999bcaa742936f3b2ec2
SuryaSrikanth <srisurya2606@gmail.com>
2021-07-05 19:11:32 +0530

    added cloudrun

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-def hello_world(request):
-    """Responds to any HTTP request.
-    Args:
-        request (flask.Request): HTTP request object.
-    Returns:
-        The response text or any set of values that can be turned into a
-        Response object using
-        `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
-    """
-    request_json = request.get_json()
-    if request.args and 'message' in request.args:
-        return request.args.get('message')
-    elif request_json and 'message' in request_json:
-        return request_json['message']
-    else:
-        return f'Hello World!'
+from flask import Flask, render_template, send_from_directory, Blueprint
+from flask_cors import CORS
+
+
+app = Flask(__name__, static_folder='angular/dist/angular')
+CORS(app)
+
+angular = Blueprint('angular', __name__,
+                    template_folder='angular/dist/angular')
+app.register_blueprint(angular)
+
+@app.route('/assets/<path:filename>')
+def custom_static_for_assets(filename):
+    return send_from_directory('angular/dist/angular/assets', filename)
+
+
+@app.route('/<path:filename>')
+def custom_static(filename):
+    return send_from_directory('angular/dist/angular/', filename)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000, debug=True)
