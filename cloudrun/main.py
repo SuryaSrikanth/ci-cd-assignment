4db63a504c3e5d473d9e20fcc556663309691cc2
SuryaSrikanth <srisurya2606@gmail.com>
2021-07-07 10:44:53 +0530

    Update main.py

--- cloudrun/main.py
+++ cloudrun/main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, send_from_directory, Blueprint
 from flask_cors import CORS
-
+import os
 
 app = Flask(__name__, static_folder='angular/dist/angular')
 CORS(app)
@@ -23,4 +23,4 @@ def index():
     return render_template("index.html")
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
